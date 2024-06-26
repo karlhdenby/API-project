a42f9cbf9e1fd901aae69cc4bd65932d72ac1bfe
karlhdenby <karlhdenby@gmail.com>
2024-06-26 09:48:07 -0700

    Successfully Seeded

--- backend/db/seeders/05-reviews.js
+++ backend/db/seeders/05-reviews.js
@@ -10,7 +10,7 @@ const { Review } = require('../models');
 const reviews = [
   {spotId: 1, userId: 2, review: "Great spot, very clean and comfortable.", stars: 5},
   {spotId: 2, userId: 3, review: "Nice location, but a bit noisy.", stars: 4},
-  {spotId: 3, userId: 4, review: "Loved the place, will visit again!", stars: 5},
+  {spotId: 3, userId: 1, review: "Loved the place, will visit again!", stars: 5},
 ];
 
 
