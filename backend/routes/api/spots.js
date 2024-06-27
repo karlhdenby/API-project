9692c074ca38f1f2022e1457fdb89e1d79fb55c7
karlhdenby <karlhdenby@gmail.com>
2024-06-27 15:28:20 -0700

    sharing

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -20,6 +20,17 @@ async function calculateAvg(id) {
     else return null
 }; 
 
+async function calculateTotal(id) {
+    const result = await Review.findAll({
+        where: {
+             spotId: id
+        }
+    })
+
+    let count = result.length
+    return count
+}; 
+
 async function findSpotImages(id) {
     const result = await SpotImage.findAll({
         where: {
@@ -62,7 +73,31 @@ async function makeSpots(array) {
     return newSpots
 };
 
-
+async function makeSpotsById(array) {
+    let newSpots = [];
+    for (let spot of array) {
+        let current = {}
+        current.id = spot.id
+        current.ownerId = spot.ownerId
+        current.address = spot.address
+        current.city = spot.city
+        current.state = spot.state
+        current.country = spot.country
+        current.lat = spot.lat
+        current.lng = spot.lng
+        current.name = spot.name
+        current.description = spot.description
+        current.price = spot.price
+        current.createdAt = (spot.createdAt)
+        current.updatedAt = (spot.updatedAt)
+        current.numReviews = await calculateTotal(spot.id)
+        current.avgStarRating = await calculateAvg(spot.id)
+        current.SpotImages = await findSpotImages(spot.id)
+        current.Owner = await findOwner(spot.ownerId)
+        newSpots.push(current);
+    };
+    return newSpots
+};
 
 
 router.get('/', async (req, res, next) => {
