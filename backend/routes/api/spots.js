6d53a5df28d6491c722dcc0c1c37ed94799ecff8
karlhdenby <karlhdenby@gmail.com>
2024-06-27 10:21:28 -0700

    sharing

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,9 +2,31 @@
 const express = require('express');
 const router = express.Router();
 const { Spot } = require('../../db/models');
+const { Review } = require('../../db/models');
+
+async function calculateAvg(spot) {
+    let total = 0;
+    let count = 0;
+    let id = spot.id;
+    let reviews = await Review.findAll({
+        where: {
+            spotId: id
+        }
+    });
+    for (let review of reviews) {
+        total = total + review.stars
+        count++
+    }
+    if(!NaN(total) && !NaN(count)) return total / count
+    else return null
+}
 
 router.get('/', async (req, res, next) => {
     const spots = await Spot.findAll();
+    for (let spot of spots) {
+        spot.avgRating = calculateAvg(spot);
+    }
+    
     return res.json({"Spots": spots});
     
 });
