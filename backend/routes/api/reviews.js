74ee017991e15fe1ddf44db93e935d57e39cceea
karlhdenby <karlhdenby@gmail.com>
2024-06-29 15:53:13 -0700

    sharing

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -40,7 +40,7 @@ router.put('/:reviewId', async (req, res, next) => {
 router.delete('/:reviewId', async (req, res, next) => {
   let reviewId = req.params.reviewId
   let review = await Review.findByPk(reviewId)
-  await review.destroy(body)
+  await review.destroy(review)
 
   res.json({"message": "Successfully deleted"})
 })
