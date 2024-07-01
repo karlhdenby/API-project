39102429cb1f46e729ecfeef75d21db5941c02c9
karlhdenby <karlhdenby@gmail.com>
2024-06-30 19:11:12 -0700

    sharing

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { Spot, Review, SpotImage, User, ReviewImage, Booking } = require("../../db/models");
+const { where } = require("sequelize");
+const { currentSpot } = require('./spots') ;
+
+router.get('/current', async (req, res, next) => {
+    const { user } = req
+
+    const bookings = Booking.findAll({
+        where: {
+            userId: user.id
+        }
+    })
+
+    return res.json(bookings)
+})
+
+module.exports = router
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,10 +8,12 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
+const bookingsRouter = require('./bookings.js');
 
 
 router.use(restoreUser);
 
+router.use('/bookings', bookingsRouter)
 
 router.use('/spots', spotsRouter);
 
