3b65d1e11fd36939354665e8004474ec179eed97
karlhdenby <karlhdenby@gmail.com>
2024-08-07 20:56:52 -0700

    sharing

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -90,41 +90,52 @@ router.get('/', (req, res) => {
 
 
 router.post('/', requireAuth, async (req, res, next) => {
-    const { credential, password } = req.body;
-
-    const user = await User.unscoped().findOne({
-        where: {
-            [Op.or]: {
-                username: credential,
-                email: credential
+    try {
+        const { credential, password } = req.body;
+    
+        const user = await User.unscoped().findOne({
+            where: {
+                [Op.or]: {
+                    username: credential,
+                    email: credential
+                }
             }
-        }
-    });
-
-    if (!user || !bcrypt.compareSync(password,
-        user.hashedPassword.toString())) {
-            const err = new Error("Login failed");
-            err.status = 401;
-            err.title = "Login failed";
-            err.errors = { credential: 'The provided credentials were invalid.'};
-            return res.status(401).json({
-                message: "Invalid credentials"
-            });
-        }
-
-        const safeUser = {
-            firstName: firstName,
-            lastName: lastName,
-            id: user.id,
-            email: user.email,
-            username: user.username,
-        };
-
-        await setTokenCookie(res, safeUser);
-
-        return res.json({
-            user: safeUser
         });
+    
+        if (!user || !bcrypt.compareSync(password,
+            user.hashedPassword.toString())) {
+                const err = new Error("Login failed");
+                err.status = 401;
+                err.title = "Login failed";
+                err.errors = { credential: 'The provided credentials were invalid.'};
+                return res.status(401).json({
+                    message: "Invalid credentials"
+                });
+            }
+    
+            const safeUser = {
+                firstName: firstName,
+                lastName: lastName,
+                id: user.id,
+                email: user.email,
+                username: user.username,
+            };
+    
+            await setTokenCookie(res, safeUser);
+    
+            return res.json({
+                user: safeUser
+            });
+        
+    } catch (error) {
+        res.status(400).json({
+            message: "Bad Request",
+            errors: {
+                credential: "Email or username is required",
+                password: "Password is required"
+            }
+        })
+    }
 });
 
 
