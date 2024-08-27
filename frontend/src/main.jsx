f75a78e6e08707e3b540191a74a430a274d30765
karlhdenby <karlhdenby@gmail.com>
2024-08-27 09:53:53 -0700

    sharing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 import { Provider } from 'react-redux';
-import configureStore from './store';
+import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
 import { Modal, ModalProvider } from './context/Modal';
